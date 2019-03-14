9b0f5a9dddbfb94a99861f56372282c1c38db8e1
Shiva Reddy <readytohackk@gmail.com>
2019-03-14 23:24:59 +0530

    Update searchapp.js

--- searchapp.js
+++ searchapp.js
@@ -5,7 +5,7 @@ class SearchApp{
   }
 // to get top youtube charts list
 async getTopAlbums(){
-  const topAlbumResponse=await fetch(`http://www.theaudiodb.com/api/v1/json/1/trending.php?country=us&type=chart_youtube_track&format=albums`);
+  const topAlbumResponse=await fetch(`https://www.theaudiodb.com/api/v1/json/1/trending.php?country=us&type=chart_youtube_track&format=albums`);
 
   const topAlbumData= await topAlbumResponse.json();
 
@@ -17,7 +17,7 @@ async getTopAlbums(){
 
 // albums are searched based on the artist name based on user input
 async getAlbumByArtistName(name){
-const albumResponseByName= await fetch(`http://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${name}`);
+const albumResponseByName= await fetch(`https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${name}`);
 const albumData = await albumResponseByName.json();
 
 return{
